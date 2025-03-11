3aac45c9125e6cfcaad67e50c8609a2300c83916
armir <aim.dev21@gmail.com>
2025-03-11 17:47:40 +0100

    small change

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddApplication();
+
 
 var app = builder.Build();
 
